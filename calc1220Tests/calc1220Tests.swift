e37996525521074a20de6b7a298e2e4232d61e37
wusu chiang <wusuchen@wusudeimac.local>
2015-12-20 14:43:16 +0800

    Initial Commit

---
+++ calc1220Tests/calc1220Tests.swift
@@ -0,0 +1,36 @@
+//
+//  calc1220Tests.swift
+//  calc1220Tests
+//
+//  Created by wusu chiang on 2015/12/20.
+//  Copyright © 2015年 wusu chiang. All rights reserved.
+//
+
+import XCTest
+@testable import calc1220
+
+class calc1220Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
